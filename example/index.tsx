91563de4df8a56f1c4a827867b87c5165e7720eb
Dave Hudson <dave@applification.net>
2021-03-13 07:59:44 +0000

    bah

--- example/index.tsx
+++ example/index.tsx
@@ -1,14 +1,28 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { useForm } from "react-hook-form";
 import { Toggle } from '../src/index';
 
+type Inputs = {
+  subscribe: boolean
+}
+
 const App = () => {
+  const { register, handleSubmit, watch, errors } = useForm<Inputs>();
+  const onSubmit = data => console.log(data);
+
+  console.log(watch("subscribed"));
+
+
   const [enabled, setEnabled] = React.useState(true);
 
   return (
     <div>
-      <Toggle enabled={enabled} handleChange={() => setEnabled(!enabled)} />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Toggle name="subscribed" enabled={enabled} handleChange={() => setEnabled(!enabled)} ref={register}  />
+        <input type="submit" value="Do something?" />
+      </form>
     </div>
   );
 };
