b793004dd2797a7aabc00746b35ab393acffeffa
Dave Hudson <dave@applification.net>
2021-03-16 08:50:33 +0000

    Error UI

--- stories/InputEmail.stories.tsx
+++ stories/InputEmail.stories.tsx
@@ -12,5 +12,10 @@ export default meta;
 const Template: Story = (args) => <InputEmail {...args} />;
 
 export const SCInputEmail = Template.bind({});
+SCInputEmail.args = { name: 'email', label: 'Your email', defaultValue: 'dave@densen.co.uk', placeholder: 'your email address' };
 
-SCInputEmail.args = { name: 'email', defaultValue: 'dave@densen.co.uk', placeholder: 'your email address' };
+export const SCInputEmailHint = Template.bind({});
+SCInputEmailHint.args = { name: 'email', label: 'Your email', placeholder: 'your email address', hintText: 'We only do SPAMMY stuff' };
+
+export const SCInputEmailError = Template.bind({});
+SCInputEmailError.args = { name: 'email', label: 'Your email', defaultValue: 'dave.co.uk', placeholder: 'your email address', error: 'You must provide a valid email' };
