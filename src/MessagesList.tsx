cf60cbe6a9c51020c1f53461d3417b4356c0a151
Dave Hudson <dave@applification.net>
2021-03-16 16:29:28 +0000

    Working TS
    but I don't like it

---
+++ src/MessagesList.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { MessageText } from './MessageText';
+import { Message } from './types/chat/Messages';
+
+
+
+
+export const MessagesList = (messages: Message[]) => {  
+  return (
+    <div className="flow-root">
+      <ul className="-mb-8">
+        {
+          Object.values(messages).map((message: Message) => {
+            return <MessageText {...message} />            
+          })
+        }
+      </ul>
+    </div>
+  )
+}
