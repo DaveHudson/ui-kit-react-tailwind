cfcd9789f67af3c6641370cbf53fe85bb1a2ff4a
Dave Hudson <dave@applification.net>
2021-03-18 20:44:32 +0000

    Story fix - wrapper for messagetext so dot doesn't show

--- stories/Message.stories.tsx
+++ stories/Message.stories.tsx
@@ -12,12 +12,17 @@ export default meta;
 const message = {
   id: '1',
   name: 'Eduardo Benz',
-  profilePic: 'https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=8&amp;w=256&amp;h=256&amp;q=80',
+  profilePic:
+    'https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=8&amp;w=256&amp;h=256&amp;q=80',
   dateStamp: '6 days ago',
-  message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt nunc ipsum tempor purus vitae id. Morbi in vestibulum nec varius. Et diam cursus quis sed purus nam.'
-}
-const Template: Story = (args) => <MessageText message={message} {...args} />;
+  message:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt nunc ipsum tempor purus vitae id. Morbi in vestibulum nec varius. Et diam cursus quis sed purus nam.',
+};
+const Template: Story = (args) => (
+  <ul className="-mb-8">
+    <MessageText message={message} {...args} />
+  </ul>
+);
 
 export const SCMessageText = Template.bind({});
 SCMessageText.args = {};
-
